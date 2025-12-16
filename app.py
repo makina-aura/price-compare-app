0e43af15358c1d42520b6411770963ef8f660588
INO-Photography <inoino.photophoto@gmail.com>
2025-12-17 03:44:46 +0900

    Add SQLite connection and save product data
    - Connect Flask to SQLite
    - Save data from /add form
    - Show saved data on /history

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect("db/comparison_app.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/dashboard")
 def dashboard():
