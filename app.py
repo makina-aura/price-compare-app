aa273c2f59b6bda78dfd4f8baaa0f5ff65566de0
MASSAN <jkrsnha20110418@gmail.com>
2025-12-17 14:45:20 +0900

    トップページ（index.html）とルーティングを追加

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
